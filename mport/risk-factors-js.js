d70710cb3a7c87add426c3734332903c83c9da88
yulricsequeira <yulrics@gmail.com>
2017-09-01 11:57:30 -0400

    Added risk factor files to mport

---
+++ mport/risk-factors-js.js
@@ -0,0 +1,6 @@
+exports.Smoking = 'Smoking';
+exports.Diet = 'Diet';
+exports.PhysicalActivity = 'PhysicalActivity';
+exports.Alcohol = 'Alcohol';
+exports.BMI = 'BMI';
+exports.Education = 'Education';
