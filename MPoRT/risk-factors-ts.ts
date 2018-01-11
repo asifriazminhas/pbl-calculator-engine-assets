cb4960bfa52cf3681a5a871f124884185745b60b
Yulric Sequeira <yulrics@gmail.com>
2018-01-11 16:33:41 -0500

    Removed MPoRT folder. Will put back in next commit

--- MPoRT/risk-factors-ts.ts
+++
@@ -1,6 +0,0 @@
-export const Smoking = 'Smoking';
-export const Diet = 'Diet';
-export const PhysicalActivity = 'PhysicalActivity';
-export const Alcohol = 'Alcohol';
-export const BMI = 'BMI';
-export const Education = 'Education';
--- MPoRT/risk-factors.js
+++
@@ -1,6 +0,0 @@
-exports.Smoking = 'Smoking';
-exports.Diet = 'Diet';
-exports.PhysicalActivity = 'PhysicalActivity';
-exports.Alcohol = 'Alcohol';
-exports.BMI = 'BMI';
-exports.Education = 'Education';
