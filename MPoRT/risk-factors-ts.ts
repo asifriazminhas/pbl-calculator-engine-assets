5a9c8ada165491758b8f3b2a08b2fa4c050ec09f
Yulric Sequeira <yulrics@gmail.com>
2018-01-11 16:34:10 -0500

    Added MPoRT folder back in

---
+++ MPoRT/risk-factors-ts.ts
@@ -0,0 +1,6 @@
+export const Smoking = 'Smoking';
+export const Diet = 'Diet';
+export const PhysicalActivity = 'PhysicalActivity';
+export const Alcohol = 'Alcohol';
+export const BMI = 'BMI';
+export const Education = 'Education';
