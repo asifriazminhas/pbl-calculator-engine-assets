94f2b504b3b196c7b095c4d4bf06cf58f6fdfc72
yulricsequeira <yulrics@gmail.com>
2019-01-24 16:27:31 -0500

    [Feature] Added typings for algorithm json file

---
+++ reference-files/index.ts
@@ -0,0 +1,7 @@
+export interface IAlgorithmJson {
+    algorithmName: string;
+    genderSpecific: boolean;
+    regressionType: 'CoxRegression';
+    maximumTime: number;
+    timeMetric: 'days' | 'years';
+}
