51d9b7fdf977a44202600a2c27c4a65dac571bfa
Yulric Sequeira <yulrics@gmail.com>
2019-05-24 15:55:14 -0400

    [Feature] Added after-success.sh script for Travis

---
+++ ci/after-success.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "projectbiglife@toh.ca"
+  git config --global user.name "BLLBot"
+}
+
+commit_website_files() {
+  git checkout -b $TRAVIS_BRANCH
+  git add .
+  git commit --message "[Feature] Algorithm Files Build"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/Big-Life-Lab/pbl-calculator-engine-assets.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin $TRAVIS_BRANCH 
+}
+
+setup_git
+commit_website_files
+upload_files
