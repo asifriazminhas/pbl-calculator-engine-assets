5cb94e1abb133a25c8bfbfc27fdcaeb0a7b50f58
Yulric Sequeira <yulrics@gmail.com>
2019-06-07 09:50:58 -0400

    [Feature] Validate model assets before converting to PMML

--- convert-to-pmml/index.ts
+++ convert-to-pmml/index.ts
@@ -1,12 +1,17 @@
-import { writePMMLFilesForModel } from "./pmml";
-import { IConfigJson } from "../reference-files";
+import { writePMMLFilesForModel } from './pmml';
+import { IConfigJson } from '../reference-files';
+import { ModelAssets } from '../src/ci/model-assets/model-assets';
 
 export async function convertToPmml() {
-  const config: IConfigJson = require("../config.json");
+    const config: IConfigJson = require('../config.json');
 
-  return Promise.all(
-    config.models.map(({ name }) => {
-      return writePMMLFilesForModel(name);
-    })
-  );
+    return Promise.all(
+        config.models
+            .filter(({ name }) => {
+                return ModelAssets.validateAssetsForModel(name);
+            })
+            .map(({ name }) => {
+                return writePMMLFilesForModel(name);
+            }),
+    );
 }
