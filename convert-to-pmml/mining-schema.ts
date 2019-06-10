80be9072ed77b9fd4a253a04f9e207b5d19bceda
Yulric Sequeira <yulrics@gmail.com>
2019-06-10 13:15:46 -0400

    [Refactor] constructMiningSchemaNode does not use hardcoded strings

--- convert-to-pmml/mining-schema.ts
+++ convert-to-pmml/mining-schema.ts
@@ -2,27 +2,30 @@ import { IMiningSchema } from '@ottawamhealth/pbl-calculator-engine/lib/parsers/
 import { InvalidValueTreatment as PmmlInvalidValueTreatment } from '@ottawamhealth/pbl-calculator-engine/lib/parsers/pmml/mining-schema/invalid-value-treatment';
 import { MissingValueTreatment as PmmlMissingValueTreatment } from '@ottawamhealth/pbl-calculator-engine/lib/parsers/pmml/mining-schema/missing-value-treatment';
 import { WebSpecV1 } from '../src/ci/model-assets/web-spec/web-spec-v1/web-spec-v1';
+import {
+    AsMissing,
+    ReturnInvalid,
+} from '../src/ci/model-assets/web-spec/web-spec-v1/invalid-value-treatment';
+import { AsMean } from '../src/ci/model-assets/web-spec/web-spec-v1/missing-value-replacement';
 
 export function constructMiningSchemaNode(
     webSpecSheet: WebSpecV1,
 ): IMiningSchema {
-    const webSpecCsv = webSpecSheet.sheet;
-
     return {
-        MiningField: webSpecCsv.map(
+        MiningField: webSpecSheet.sheet.map(
             ({ InvalidValueTreatment, Name, MissingValueReplacement }) => {
                 return {
                     $: Object.assign(
                         {
                             name: Name,
                             invalidValueTreatment:
-                                InvalidValueTreatment === 'asMissing'
+                                InvalidValueTreatment === AsMissing
                                     ? PmmlInvalidValueTreatment.AsMissing
-                                    : InvalidValueTreatment === 'returnInvalid'
+                                    : InvalidValueTreatment === ReturnInvalid
                                     ? PmmlInvalidValueTreatment.ReturnInvalid
                                     : PmmlInvalidValueTreatment.AsIs,
                         },
-                        MissingValueReplacement === 'asMean'
+                        MissingValueReplacement === AsMean
                             ? {
                                   missingValueTreatment:
                                       PmmlMissingValueTreatment.AsMean,
