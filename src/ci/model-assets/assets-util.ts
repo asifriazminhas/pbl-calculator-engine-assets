55fadcb3b644107dad67f27f9188b0a5e2cdc95d
yulricsequeira <yulrics@gmail.com>
2019-06-11 13:25:00 -0400

    [Feature[ Added parseXmlFile method to AssetsUtil

--- src/ci/model-assets/assets-util.ts
+++ src/ci/model-assets/assets-util.ts
@@ -1,6 +1,14 @@
 import * as path from 'path';
 import { readFileSync } from 'fs';
 import csvParse from 'csv-parse/lib/sync';
+import { promisify } from 'bluebird';
+import { parseString, convertableToString, OptionsV2 } from 'xml2js';
+// xml2js has 2 types for the same function name (parseString) and we want the second type (the one with the options argument). But when promisifying the function the type returned will be the first type promisified, thus we have to explicitly set the type of the promisified parseString
+const promisifiedParseString = promisify(parseString as (
+    xml: convertableToString,
+    options: OptionsV2,
+    callback: (err: any, result: any) => void,
+) => void);
 
 export abstract class AssetsUtil {
     static getAssetsFolderPath(modelName: string) {
@@ -10,6 +18,14 @@ export abstract class AssetsUtil {
     static parseCsvFile(filePath: string) {
         return csvParse(readFileSync(filePath, 'utf8'), {
             columns: true,
+        });
+    }
+
+    static parseXmlFile(filePath: string) {
+        return promisifiedParseString(readFileSync(filePath, 'utf8'), {
+            explicitArray: false,
+            explicitChildren: true,
+            preserveChildrenOrder: true,
         });
     }
 }
